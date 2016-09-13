b8513b318f45a69920ce977c6354fed474049bc5
PC\User <stephendolman2112@gmail.com>
2016-09-13 22:08:57 +0100

    Fixed horrendous bug in which looking at the snek counts as your draft pick.

--- Gimmix/src/snakeDraftManager/Table.java
+++ Gimmix/src/snakeDraftManager/Table.java
@@ -286,6 +286,7 @@ public class Table {
 			int pick = sc.nextInt();
 			if (pick == 999) {
 				printSnekAndPools();
+				askPlayerToPickOne(p, tier);
 			} else {
 				p.claimsPick(tier.remove(pick - 1));
 			}
@@ -299,20 +300,19 @@ public class Table {
 
 	private static void printSnekAndPools() {
 		String padding = "                ";
-		String snek = rpad(padding, 13) + "Y" + '\n';
-		snek +=rpad(padding, 11) + "/' '\\" + '\n';
-		snek += rpad(padding, 10) +"|     |" + '\n';
-		snek += rpad(padding, 11) +"\\   /" + '\n';
+		String snek = rpad(padding, 12) + "_Y_" + '\n';
+		snek += rpad(padding, 11) + "/' '\\" + '\n';
+		snek += rpad(padding, 10) + "|     |" + '\n';
+		snek += rpad(padding, 11) + "\\   /" + '\n';
 		snek += rpad(padding, 11) + "|  |" + '\n';
 
 		for (Player p : players) {
 			snek += rpad(p.getName() + padding, 11) + "|  |  " + p.getPoolAsString() + '\n';
 			snek += rpad(padding, 11) + "|  |" + '\n';
-			snek += rpad(padding, 11) + "|  |" + '\n';
 		}
-		
-		snek += rpad(padding, 11) + "\\_/" + '\n';
-		
+
+		snek += rpad(padding, 11) + "\\__/" + '\n';
+
 		System.out.println(snek);
 
 	}
