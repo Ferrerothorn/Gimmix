c921859ff91639a76bf227a33598d7be112d556b
Stephen Dolman <sdolman@sabio.co.uk>
2017-03-17 11:20:54 +0000

    Removing Deck Metagame package from Swiss.

--- Deck Metagame/factory/Custom.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-public class Custom extends Deck {
-
-	public Custom(int aggro, int control, int combo, int midrange) {
-		this.setArchetype("Custom");
-		this.setStats(aggro, control, combo, midrange);
-	}
-}
