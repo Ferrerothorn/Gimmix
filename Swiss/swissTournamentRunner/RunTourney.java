83e6a66c9abde4f91f79a3a0c3809ddd9bee7f13
PC\User <stephendolman2112@gmail.com>
2017-04-07 12:48:14 +0100

    Version updated.

--- Swiss/swissTournamentRunner/RunTourney.java
+++ Swiss/swissTournamentRunner/RunTourney.java
@@ -37,7 +37,7 @@ public class RunTourney {
 
 	private static void showCredits() {
 		GUI.postString("Welcome to B-T-C, the Swiss Tournament Bracket Organiser!");
-		GUI.postString("(Version 7.0.1 - Made by Steve Dolman)");
+		GUI.postString("(Version 7.1.1 - Made by Steve Dolman)");
 		GUI.postString("Shoutout to Rachel Dolman and Darren Macey for help in testing and debug.");
 		GUI.postString();
 	}
