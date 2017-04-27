920901d8709c1ecf94472394206d0714a7aa30c9
Stephen Dolman <sdolman@sabio.co.uk>
2017-04-27 08:57:43 +0100

    Update name of output box.

--- Swiss/swissTournamentRunner/Tournament.java
+++ Swiss/swissTournamentRunner/Tournament.java
@@ -205,7 +205,7 @@ public class Tournament {
 
 			try {
 				GUI.printCurrentBattles(currentBattles, roundString);
-				GUI.pairingsBox.setCaretPosition(GUI.pairingsBox.getDocument().getLength());
+				GUI.textOutputBox.setCaretPosition(GUI.textOutputBox.getDocument().getLength());
 
 				waitForUserInput();
 
