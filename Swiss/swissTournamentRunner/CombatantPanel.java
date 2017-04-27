1143d4d0a8338452294b79a8c2d4d5297a6e5f45
Stephen Dolman <sdolman@sabio.co.uk>
2017-04-27 15:38:07 +0100

    We need to track tourneys in the panel in order to pass the button value clicked through.

--- Swiss/swissTournamentRunner/CombatantPanel.java
+++ Swiss/swissTournamentRunner/CombatantPanel.java
@@ -7,16 +7,17 @@ import javax.swing.JPanel;
 
 public class CombatantPanel extends JPanel {
 
-	public CombatantPanel(ArrayList<Battle> currentBattles) {
+	public CombatantPanel(ArrayList<Battle> currentBattles, Tournament t) {
 		setLayout(new java.awt.GridLayout(currentBattles.size(), 2));
 		for (Battle b : currentBattles) {
 			JButton playerButton = new JButton(b.getP1().getName());
 			playerButton.addActionListener(new java.awt.event.ActionListener() {
 				public void actionPerformed(java.awt.event.ActionEvent e) {
-					System.out.println(playerButton.getText());
+					t.reportBattleWinner(playerButton.getText());
 				}
 			});
 			add(playerButton);
 		}
+		this.setVisible(true);
 	}
 }
